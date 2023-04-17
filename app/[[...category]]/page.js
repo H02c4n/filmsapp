e7280186f0e067b5ea7fe10737216c2841c70cc8
halil <svtr.hozcan@gmail.com>
2023-04-17 11:22:09 +0200

    finished

--- app/[[...category]]/page.js
+++ app/[[...category]]/page.js
@@ -3,7 +3,12 @@ import HomeContainer from "@/containers/home";
 
 import Movies from "@/mocks/movies.json";
 
-function HomePage({ params }) {
+async function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function HomePage({ params }) {
+  await delay(20000);
   let selectedCategory;
 
   if (params.category?.length > 0) {
